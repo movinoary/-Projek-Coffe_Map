bc12539b58c1269b0b6113169f74c7576d89917a
movinoary <97898853+movinoary@users.noreply.github.com>
2025-02-18 23:55:35 +0700

    add progres list map

--- src/class/list.jsx
+++ src/class/list.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dataPhoto } from "../assets/data/dataPhoto";
 import Card from "../components/card";
+import LoadingBar from "react-top-loading-bar";
 
 const List = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [progress, setProgress] = useState(1);
+
   const width = window.innerWidth;
   const postsPerPage = width >= 1650 ? 8 : 6;
 
@@ -17,6 +20,28 @@ const List = () => {
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pages.push(i);
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (currentPage === pages.length) {
+        setCurrentPage(1);
+      } else {
+        setCurrentPage(currentPage + 1);
+      }
+    }, 5000);
+  }, [currentPage]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (progress === 4) {
+        setProgress(1);
+      } else {
+        setProgress(progress + 1);
+      }
+    }, 1250);
+  }, [currentPage, progress]);
+
+  console.log(progress);
 
   const addPage = (number) => setCurrentPage(number);
 
@@ -53,6 +78,31 @@ const List = () => {
             </button>
           ))}
         </div>
+        <div className="loader">
+          <div
+            className={`${
+              progress === 1
+                ? "satu"
+                : progress === 2
+                ? "dua"
+                : progress === 3
+                ? "tiga"
+                : progress === 4
+                ? "empat"
+                : null
+            }`}
+          />
+        </div>
+        {/* <div>
+          <LoadingBar
+            color="#f11946"
+            progress={progress}
+            onLoaderFinished={() => setProgress(0)}
+          />
+          <button onClick={() => setProgress(progress + 10)}>Add 10%</button>
+          <button onClick={() => setProgress(progress + 20)}>Add 20%</button>
+          <button onClick={() => setProgress(100)}>Complete</button>
+        </div> */}
       </div>
     </section>
   );
